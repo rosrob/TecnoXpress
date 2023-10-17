8b913cd3f2f27e5c4236ed71be5a8bb8a719ef3c
RominaTejeda <tejeda.romina@hotmail.com>
2023-10-17 17:51:30 -0300

    modificaciones css

---
+++ FrontEnd/js_carrito/menu.js
@@ -0,0 +1,10 @@
+
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () => {
+    aside.classList.add("aside-visible");
+})
+
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+})
