49e0df0104885dec88d7531847c8a3b2e2638bf5
RominaTejeda <tejeda.romina@hotmail.com>
2023-10-18 11:12:05 -0300

    continuo modficandocss

--- FrontEnd/js_carrito/menu.js
+++ FrontEnd/js_carrito/menu.js
@@ -1,4 +1,5 @@
-
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
 const aside = document.querySelector("aside");
 
 openMenu.addEventListener("click", () => {
